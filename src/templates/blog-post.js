e470db4c40fd829cf9461894b74982ef1450a7f0
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-04-18 13:52:07 +0800

    fix component relative path

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,7 @@ import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import NextPrev from "../components/NextPrev"
+import NextPrev from "../components/nextPrev"
 
 const Styled = styled.article`
   ul {
